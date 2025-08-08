7326a169434d6eb9acd26401d3aa88729c953eab
Kost0 <lavrenov.konstantin@gmail.com>
2025-08-08 13:44:15 +0300

    Add frontend

--- apps/backend/cmd/main.go
+++ apps/backend/cmd/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
-	"github.com/Kost0/L0/apps/backend/internal/repository"
+	"fmt"
+	"github.com/Kost0/L0/internal/repository"
 	"log"
+	"net/http"
 )
 
 func main() {
-	db, err := repository.CreateDataBase()
+	db, err := repository.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Starting server")
 	defer db.Close()
 
 	err = repository.RunMigrations(db, "orders_l0")
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Migrations complete")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ apps/backend/internal/repository/connectDB.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
