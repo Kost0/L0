1177580303ecc721ff6ff0b4c31cea4e5cf10288
Kost0 <lavrenov.konstantin@gmail.com>
2025-08-10 14:25:14 +0300

    Add saving to db

--- apps/backend/internal/models/models.go
+++ apps/backend/internal/models/models.go
@@ -3,58 +3,65 @@ package models
 import "time"
 
 type Order struct {
-	orderUID          [16]byte  `json:"orderUID"`
-	trackNumber       string    `json:"trackNumber"`
-	entry             string    `json:"entry"`
-	deliveryID        [16]byte  `json:"deliveryID"`
-	paymentID         [16]byte  `json:"paymentID"`
-	locale            string    `json:"locale"`
-	internalSignature string    `json:"internalSignature"`
-	customerID        string    `json:"customerID"`
-	deliveryService   string    `json:"deliveryService"`
-	shardkey          string    `json:"shardKey"`
-	smID              int       `json:"smID"`
-	dateCreated       time.Time `json:"dateCreated"`
-	oofShard          string    `json:"oofShard"`
+	OrderUID          [16]byte  `json:"orderUID"`
+	TrackNumber       string    `json:"trackNumber"`
+	Entry             string    `json:"entry"`
+	DeliveryID        [16]byte  `json:"deliveryID"`
+	PaymentID         [16]byte  `json:"paymentID"`
+	Locale            string    `json:"locale"`
+	InternalSignature string    `json:"internalSignature"`
+	CustomerID        string    `json:"customerID"`
+	DeliveryService   string    `json:"deliveryService"`
+	Shardkey          string    `json:"shardKey"`
+	SmID              int       `json:"smID"`
+	DateCreated       time.Time `json:"dateCreated"`
+	OofShard          string    `json:"oofShard"`
 }
 
 type Delivery struct {
-	id      [16]byte `json:"id"`
-	name    string   `json:"name"`
-	phone   string   `json:"phone"`
-	zip     string   `json:"zip"`
-	city    string   `json:"city"`
-	address string   `json:"address"`
-	region  string   `json:"region"`
-	email   string   `json:"email"`
+	ID      [16]byte `json:"id"`
+	Name    string   `json:"name"`
+	Phone   string   `json:"phone"`
+	Zip     string   `json:"zip"`
+	City    string   `json:"city"`
+	Address string   `json:"address"`
+	Region  string   `json:"region"`
+	Email   string   `json:"email"`
 }
 
 type Payment struct {
-	id           [16]byte `json:"id"`
-	transaction  string   `json:"transaction"`
-	requestID    string   `json:"requestID"`
-	currency     string   `json:"currency"`
-	provider     string   `json:"provider"`
-	amount       int      `json:"amount"`
-	paymentDT    int      `json:"paymentDT"`
-	bank         string   `json:"bank"`
-	deliveryCost int      `json:"deliveryCost"`
-	goodsTotal   int      `json:"goodsTotal"`
-	customFee    int      `json:"customFee"`
+	ID           [16]byte `json:"id"`
+	Transaction  string   `json:"transaction"`
+	RequestID    string   `json:"requestID"`
+	Currency     string   `json:"currency"`
+	Provider     string   `json:"provider"`
+	Amount       int      `json:"amount"`
+	PaymentDT    int      `json:"paymentDT"`
+	Bank         string   `json:"bank"`
+	DeliveryCost int      `json:"deliveryCost"`
+	GoodsTotal   int      `json:"goodsTotal"`
+	CustomFee    int      `json:"customFee"`
 }
 
 type Item struct {
-	id          [16]byte `json:"id"`
-	orderUID    [16]byte `json:"orderUID"`
-	chrtID      int      `json:"chrtID"`
-	trackNumber string   `json:"trackNumber"`
-	price       int      `json:"price"`
-	rid         string   `json:"rid"`
-	name        string   `json:"name"`
-	sale        int      `json:"sale"`
-	size        string   `json:"size"`
-	totalPrice  int      `json:"totalPrice"`
-	nmID        int      `json:"nmID"`
-	brand       string   `json:"brand""`
-	status      int      `json:"status"`
+	ID          [16]byte `json:"id"`
+	OrderUID    [16]byte `json:"orderUID"`
+	ChrtID      int      `json:"chrtID"`
+	TrackNumber string   `json:"trackNumber"`
+	Price       int      `json:"price"`
+	Rid         string   `json:"rid"`
+	Name        string   `json:"name"`
+	Sale        int      `json:"sale"`
+	Size        string   `json:"size"`
+	TotalPrice  int      `json:"totalPrice"`
+	NmID        int      `json:"nmID"`
+	Brand       string   `json:"brand"`
+	Status      int      `json:"status"`
+}
+
+type CombinedData struct {
+	Order    Order
+	Payment  Payment
+	Delivery Delivery
+	Items    []Item
 }
--- apps/backend/internal/repository/operators.go
+++ apps/backend/internal/repository/operators.go
@@ -3,23 +3,235 @@ package repository
 import (
 	"database/sql"
 	"encoding/json"
+
 	"github.com/Kost0/L0/internal/models"
 	"github.com/segmentio/kafka-go"
 )
 
-type CombinedData struct {
-	order    models.Order
-	payment  models.Payment
-	delivery models.Delivery
-	items    []models.Item
-}
-
 func InsertOrder(db *sql.DB, msg *kafka.Message) error {
-	var data CombinedData
+	var data models.CombinedData
 	err := json.Unmarshal(msg.Value, &data)
 	if err != nil {
 		return err
 	}
 
+	delivery := data.Delivery
+	payment := data.Payment
+	order := data.Order
+	items := data.Items
+
+	queryDelivery := `
+INSERT INTO delivery (
+    id,
+    name,
+    phone,
+    zip,
+    city,
+    address,
+    region,
+    email
+) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
+`
+
+	queryPayment := `
+INSERT INTO payments (
+    id,
+    transaction,
+    request_id,
+    currency,
+    provider,
+    amount,
+    payment_dt,
+    bank,
+    delivery_cost,
+    goods_total,
+    custom_fee
+) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`
+
+	queryOrder := `
+INSERT INTO orders (
+    order_uid,
+    track_number,
+    entry,
+    delivery_id,
+    payment_id,
+    locale,
+    internal_signature,
+    customer_id,
+    delivery_service,
+    shardkey,
+    sm_id,
+    date_created,
+    oof_shard
+) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
+`
+
+	queryItem := `
+INSERT INTO items (
+	order_uid,
+    chrt_id,
+    track_number,
+    price,
+    rid,
+    name,
+    sale,
+    size,
+    total_price,
+    nm_id,
+    brand,
+    status
+) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);`
+
+	_, err = db.Exec(queryDelivery,
+		delivery.ID,
+		delivery.Name,
+		delivery.Phone,
+		delivery.Zip,
+		delivery.City,
+		delivery.Address,
+		delivery.Region,
+		delivery.Email,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(queryPayment,
+		payment.ID,
+		payment.Transaction,
+		payment.RequestID,
+		payment.Currency,
+		payment.Provider,
+		payment.Amount,
+		payment.Bank,
+		payment.DeliveryCost,
+		payment.GoodsTotal,
+		payment.CustomFee,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(queryOrder,
+		order.OrderUID,
+		order.TrackNumber,
+		order.Entry,
+		order.PaymentID,
+		order.DeliveryID,
+		order.Locale,
+		order.InternalSignature,
+		order.CustomerID,
+		order.DeliveryService,
+		order.Shardkey,
+		order.SmID,
+		order.DateCreated,
+		order.OofShard,
+	)
+	if err != nil {
+		return err
+	}
+
+	for _, i := range items {
+		db.Exec(queryItem,
+			i.OrderUID,
+			i.ChrtID,
+			i.TrackNumber,
+			i.Price,
+			i.Rid,
+			i.Name,
+			i.Sale,
+			i.Size,
+			i.TotalPrice,
+			i.NmID,
+			i.Brand,
+			i.Status,
+		)
+	}
 	return nil
+}
+
+func SelectOrder(db *sql.DB, orderUID string) (*models.CombinedData, error) {
+	order := models.Order{}
+	delivery := models.Delivery{}
+	payment := models.Payment{}
+	items := []models.Item{}
+
+	queryOrder := `SELECT * FROM orders WHERE order_uid = $1`
+	row := db.QueryRow(queryOrder, orderUID)
+	err := row.Scan(order.OrderUID,
+		order.TrackNumber,
+		order.Entry,
+		order.PaymentID,
+		order.DeliveryID,
+		order.Locale,
+		order.InternalSignature,
+		order.CustomerID,
+		order.DeliveryService,
+		order.Shardkey,
+		order.SmID,
+		order.DateCreated,
+		order.OofShard,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	queryDelivery := `SELECT * FROM delivery
+INNER JOIN orders ON delivery.order_uid = orders.order_uid`
+	row = db.QueryRow(queryDelivery, orderUID)
+	err = row.Scan(delivery.ID,
+		delivery.Name,
+		delivery.Phone,
+		delivery.Zip,
+		delivery.City,
+		delivery.Address,
+		delivery.Region,
+		delivery.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	queryPayment := `SELECT * FROM payment
+INNER JOIN orders ON payment.order_uid = orders.order_uid`
+	row = db.QueryRow(queryPayment, orderUID)
+	err = row.Scan(payment.ID,
+		payment.Transaction,
+		payment.RequestID,
+		payment.Currency,
+		payment.Provider,
+		payment.Amount,
+		payment.Bank,
+		payment.DeliveryCost,
+		payment.GoodsTotal,
+		payment.CustomFee)
+
+	queryItems := `SELECT * FROM items WHERE order_uid = $1`
+	rows, err := db.Query(queryItems, orderUID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		item := models.Item{}
+		err = rows.Scan(item.OrderUID,
+			item.ChrtID,
+			item.TrackNumber,
+			item.Price,
+			item.Rid,
+			item.Name,
+			item.Sale,
+			item.Size,
+			item.TotalPrice,
+			item.NmID,
+			item.Brand,
+			item.Status)
+		if err != nil {
+			return nil, err
+		}
+		items = append(items, item)
+	}
+
+	data := models.CombinedData{order, payment, delivery, items}
+	return &data, nil
 }
